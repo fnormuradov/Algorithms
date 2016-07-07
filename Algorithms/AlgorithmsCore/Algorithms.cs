5fef3292b31eb401818e93a02de69412648c1077
pharrukh <farrukh.normuradov@gmail.com>
2016-07-07 23:18:32 +0500

    Week2: added RSA

--- Algorithms/AlgorithmsCore/Algorithms.cs
+++ Algorithms/AlgorithmsCore/Algorithms.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,47 @@ namespace AlgorithmsCore
 {
     public static partial class Algorithms
     {
+        public static int[] GetPrimeNumbers(int n)
+        {
+            var list = new List<int>();
+            list.AddRange(new List<int>() { 2, 3, 5, 7, 11 });
+            var possiblePrime = list.Last();
+            var i = 0;
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            do
+            {
+                possiblePrime += 2;
+                bool isDivisible = false;
+                foreach (var prime in list)
+                {
+                    //Console.WriteLine("prime: " + prime);
+                    //Console.WriteLine("possible prime: " + possiblePrime);
+                    if (possiblePrime % prime == 0)
+                    {
+                        isDivisible = true;
+                        break;
+                    }
+                }
+                if (isDivisible == false)
+                {
+                    list.Add(possiblePrime);
+                    i++;
+                }
+
+            } while (i < n - 1);
+            Console.WriteLine("Number of primes found: " + list.Count());
+
+            stopwatch.Stop();
+            var milliSeocnds = stopwatch.ElapsedMilliseconds;
+
+            Console.WriteLine(possiblePrime + " | " + milliSeocnds + " milliseconds.");
+
+            System.Console.ReadKey();
+            return list.ToArray();
+        }
 
     }
 }
--- Algorithms/Console/Program.cs
+++ Algorithms/Console/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using AlgorithmsCore;
 
 namespace Console
 {
@@ -10,6 +13,10 @@ namespace Console
     {
         static void Main(string[] args)
         {
+            //System.Console.SetOut(new StreamWriter("C:/Users/Farrukh/Output.txt"));
+            //var result = Algorithms.GetPrimeNumbers(100000);
+            System.Console.WriteLine(BigInteger.Max(10000000000000000,10000000000000000));
+            System.Console.ReadKey();
         }
     }
 }
