820a614e9279612ea3737c5f8276f3f94cc71e27
pharrukh <farrukh.normuradov@gmail.com>
2016-07-09 14:01:34 +0500

    Added Factorization

--- Algorithms/AlgorithmsCore/Algorithms.cs
+++ Algorithms/AlgorithmsCore/Algorithms.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,16 +10,19 @@ namespace AlgorithmsCore
 {
     public static partial class Algorithms
     {
-        public static int[] GetPrimeNumbers(int n)
+        /// <summary>
+        /// Returns first n prime numbers
+        /// </summary>
+        /// <param name="n">The last nth prime BigIntegerex</param>
+        /// <returns></returns>
+        public static BigInteger[] GetPrimeNumbers(BigInteger n)
         {
-            var list = new List<int>();
-            list.AddRange(new List<int>() { 2, 3, 5, 7, 11 });
+            var list = new List<BigInteger>();
+            list.AddRange(new List<BigInteger>() { 2, 3, 5, 7, 11 });
             var possiblePrime = list.Last();
             var i = 0;
-            Stopwatch stopwatch = new Stopwatch();
-
-            stopwatch.Start();
-
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
             do
             {
                 possiblePrime += 2;
@@ -40,16 +44,60 @@ namespace AlgorithmsCore
                 }
 
             } while (i < n - 1);
-            Console.WriteLine("Number of primes found: " + list.Count());
+            //Console.WriteLine("Number of primes found: " + list.Count());
 
-            stopwatch.Stop();
-            var milliSeocnds = stopwatch.ElapsedMilliseconds;
+            //stopwatch.Stop();
+            //var milliSeocnds = stopwatch.ElapsedMilliseconds;
 
-            Console.WriteLine(possiblePrime + " | " + milliSeocnds + " milliseconds.");
+            // Console.WriteLine(possiblePrime + " | " + milliSeocnds + " milliseconds.");
 
-            System.Console.ReadKey();
+            //System.Console.ReadKey();
             return list.ToArray();
         }
 
+        /// <summary>
+        /// Returns first n prime numbers
+        /// </summary>
+        /// <param name="n">The last nth prime BigIntegerex</param>
+        /// <returns></returns>
+        public static BigInteger[] GetPrimeNumbersTillGiven(BigInteger n)
+        {
+            var list = new List<BigInteger>();
+            list.AddRange(new List<BigInteger>() { 2, 3 });
+            var possiblePrime = list.Last();
+            var i = 0;
+            //Stopwatch stopwatch = new Stopwatch();
+            //stopwatch.Start();
+            while (possiblePrime < n)
+            {
+                possiblePrime += 2;
+                bool isDivisible = false;
+                foreach (var prime in list)
+                {
+                    //Console.WriteLine("prime: " + prime);
+                    //Console.WriteLine("possible prime: " + possiblePrime);
+                    if (possiblePrime % prime == 0)
+                    {
+                        isDivisible = true;
+                        break;
+                    }
+                }
+                if (isDivisible == false)
+                {
+                    list.Add(possiblePrime);
+                    i++;
+                }
+
+            }
+            //Console.WriteLine("Number of primes found: " + list.Count());
+
+            //stopwatch.Stop();
+            //var milliSeocnds = stopwatch.ElapsedMilliseconds;
+
+            // Console.WriteLine(possiblePrime + " | " + milliSeocnds + " milliseconds.");
+
+            //System.Console.ReadKey();
+            return list.ToArray();
+        }
     }
 }
