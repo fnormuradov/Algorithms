3c206e295abe7e6da4a025b0a5603866a721f689
pharrukh <farrukh.normuradov@gmail.com>
2016-07-12 22:27:33 +0500

    Week3:solved some euler problems

--- Algorithms/AlgorithmTests/StringHelperTests.cs
+++ Algorithms/AlgorithmTests/StringHelperTests.cs
@@ -22,5 +22,89 @@ namespace AlgorithmTests
                 Assert.AreEqual(expectedStrings[i], resultingString);
             }
         }
+
+        [TestMethod]
+        public void GetDigitsTest()
+        {
+            Assert.AreEqual(1, StringHelpers.GetDigits(11));
+            Assert.AreEqual(2, StringHelpers.GetDigits(1112));
+            Assert.AreEqual(6, StringHelpers.GetDigits(115316));
+        }
+        [TestMethod]
+        public void GetDecatesTest()
+        {
+            Assert.AreEqual(1, StringHelpers.GetDecates(11));
+            Assert.AreEqual(1, StringHelpers.GetDecates(1112));
+            Assert.AreEqual(1, StringHelpers.GetDecates(115316));
+        }
+        [TestMethod]
+        public void GetHunderdsTest()
+        {
+            Assert.AreEqual(2, StringHelpers.GetHundreds(222));
+            Assert.AreEqual(3, StringHelpers.GetHundreds(322));
+            Assert.AreEqual(9, StringHelpers.GetHundreds(115916));
+        }
+        private List<Tuple<int, string>> Spellings = new List<Tuple<int, string>>()
+        {
+            new Tuple<int, string>(1,"one"),
+            new Tuple<int, string>(2,"two"),
+            new Tuple<int, string>(3,"three"),
+            new Tuple<int, string>(4,"four"),
+            new Tuple<int, string>(5,"five"),
+            new Tuple<int, string>(6,"six"),
+            new Tuple<int, string>(7,"seven"),
+            new Tuple<int, string>(8,"eight"),
+            new Tuple<int, string>(9,"nine"),
+            new Tuple<int, string>(10,"ten"),
+            new Tuple<int, string>(11,"eleven"),
+            new Tuple<int, string>(12,"twelve"),
+            new Tuple<int, string>(13,"thirteen"),
+            new Tuple<int, string>(14,"fourteen"),
+            new Tuple<int, string>(15,"fifteen"),
+            new Tuple<int, string>(16,"sixteen"),
+            new Tuple<int, string>(17,"seventeen"),
+            new Tuple<int, string>(18,"eighteen"),
+            new Tuple<int, string>(19,"nineteen"),
+            new Tuple<int, string>(20,"twenty"),
+            new Tuple<int, string>(30,"thirty"),
+            new Tuple<int, string>(21,"twenty-one"),
+            new Tuple<int, string>(22,"twenty-two"),
+            new Tuple<int, string>(23,"twenty-three"),
+            new Tuple<int, string>(34,"thirty-four"),
+            new Tuple<int, string>(45,"forty-five"),
+            new Tuple<int, string>(40,"forty"),
+            new Tuple<int, string>(56,"fifty-six"),
+            new Tuple<int, string>(50,"fifty"),
+            new Tuple<int, string>(67,"sixty-seven"),
+            new Tuple<int, string>(60,"sixty"),
+            new Tuple<int, string>(78,"seventy-eight"),
+            new Tuple<int, string>(70,"seventy"),
+            new Tuple<int, string>(89,"eighty-nine"),
+            new Tuple<int, string>(80,"eighty"),
+            new Tuple<int, string>(98,"ninety-eight"),
+            new Tuple<int, string>(90,"ninety"),
+            new Tuple<int, string>(100,"one hundred"),
+            new Tuple<int, string>(101,"one hundred and one"),
+            new Tuple<int, string>(120,"one hundred and twenty"),
+            new Tuple<int, string>(200,"two hundred"),
+            new Tuple<int, string>(300,"three hundred"),
+            new Tuple<int, string>(568,"five hundred and sixty-eight"),
+            new Tuple<int, string>(1000,"one thousand"),
+        };
+        [TestMethod]
+        public void TestRunner()
+        {
+            foreach (var tuple in Spellings)
+            {
+                var number = tuple.Item1;
+                var spelling = tuple.Item2;
+                GenericWordTest(number, spelling);
+            }
+        }
+        public void GenericWordTest(int n, string spelling)
+        {
+            var resultSpelling = StringHelpers.SpellNumber(n);
+            Assert.AreEqual(spelling, resultSpelling);
+        }
     }
 }
--- Algorithms/AlgorithmsCore/StringHelpers.cs
+++ Algorithms/AlgorithmsCore/StringHelpers.cs
@@ -14,5 +14,116 @@ namespace AlgorithmsCore
             Array.Reverse(charArray);
             return new string(charArray);
         }
+
+        public static string SpellNumber(int n)
+        {
+            if (n <= 15 || n == 18)
+                return SpellSimpleNumber(n);
+            if (n < 20)
+                return SpellSimpleNumber(int.Parse(n.ToString().Substring(1))) + "teen";
+            if (n >= 20 && GetNumberOfDigits(n) > 1 && n < 100)
+            {
+                var digit = SpellSimpleNumber(GetDigits(n));
+                if (digit != "")
+                    return SpellDecades(GetDecates(n)) + "-" + digit;
+                return SpellDecades(GetDecates(n));
+            }
+            if (n >= 100 && n < 1000)
+            {
+                var spelledDecate = SpellDecades(GetDecates(n));
+                var spelledDigit = SpellSimpleNumber(GetDigits(n));
+                if (spelledDigit == "" && spelledDecate == "")
+                    return SpellSimpleNumber(GetHundreds(n)) + " hundred";
+                return SpellSimpleNumber(GetHundreds(n)) + " hundred and " + SpellNumber(n % 100);
+            }
+            if (n == 1000)
+                return "one thousand";
+            throw new Exception("Out of range");
+        }
+
+        public static string SpellSimpleNumber(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return "";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 18:
+                    return "eighteen";
+            }
+            throw new Exception("I do not know this number");
+        }
+        public static string SpellDecades(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return "";
+                case 2:
+                    return "twenty";
+                case 3:
+                    return "thirty";
+                case 4:
+                    return "forty";
+                case 5:
+                    return "fifty";
+                case 8:
+                    return "eighty";
+                default:
+                    return SpellSimpleNumber(n) + "ty";
+            }
+        }
+        private static int GetNumberOfDigits(int n)
+        {
+            return n.ToString().Length;
+        }
+
+        public static int GetDecates(int n)
+        {
+            var len = n.ToString().Length;
+            if (GetNumberOfDigits(n) > 1)
+                return int.Parse(n.ToString().Substring(len - 2, 1));
+            throw new Exception("More than 2 digits present");
+        }
+        public static int GetDigits(int n)
+        {
+            return n % 10;
+        }
+        public static int GetHundreds(int n)
+        {
+            var len = n.ToString().Length;
+            if (GetNumberOfDigits(n) > 2)
+                return int.Parse(n.ToString().Substring(len - 3, 1));
+            throw new Exception("More than 3 digits present");
+        }
     }
 }
