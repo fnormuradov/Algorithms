5eafb93cd7edf7a5c19243c15a672653161b7bef
pharrukh <farrukh.normuradov@gmail.com>
2016-07-22 19:10:46 +0500

    Week3-2: added some solutions

--- Algorithms/ProjectEulerTests/Problem9.cs
+++ Algorithms/ProjectEulerTests/Problem9.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ProjectEulerTests
