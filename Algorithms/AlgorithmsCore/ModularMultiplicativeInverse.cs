203237556701c1090e5a3973a02d37c755286674
pharrukh <farrukh.normuradov@gmail.com>
2016-07-23 02:15:00 +0500

    Week4-ClassWork: removed all logic

--- Algorithms/AlgorithmsCore/ModularMultiplicativeInverse.cs
+++ Algorithms/AlgorithmsCore/ModularMultiplicativeInverse.cs
@@ -19,13 +19,11 @@ namespace AlgorithmsCore
         public static BigInteger GetModularMultiplicativeInverse(BigInteger a, BigInteger m)
         {
             //Verify that a and m are coprime
-            if (EuclideanAlgorithm(a, m) != 1)
-                //If they are not - the inverse does not exist. Throw an exception.
-                throw new Exception($"Modular Multiplicative Inverse does not exist for this pair of numbers: {a} and {m}");
+            //If they are not - the inverse does not exist. Throw an exception.
             //In other cases return the last element in the array returned by ExtendedEuclideanAlgorithm method, which is the Modular Multiplicative Inverse
-            var result = ExtendedEuclideanAlgorithm(a, m);
-            return result[2];
+            throw new NotImplementedException();
         }
+
         /// <summary>
         /// See: https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
         /// </summary>
@@ -36,56 +34,26 @@ namespace AlgorithmsCore
         public static BigInteger[] ExtendedEuclideanAlgorithm(BigInteger a, BigInteger b)
         {
             //Prepare the array to store x, y and r
-            BigInteger[] result = new BigInteger[3];
             // Select min and max numbers so that division is correct
             // If a less than b, switch them
-            if (a < b)
-            {
-                BigInteger temp = a;
-                a = b;
-                b = temp;
-            }
+
             //Prepare the following integers r(=b), q(=0), x_0(=1), y_0(=0)
             //x_1(=0), y_1(=1), x(=0), y(=0)
-            BigInteger r = b;
-            BigInteger q = 0;
-            BigInteger x_0 = 1;
-            BigInteger y_0 = 0;
-            BigInteger x_1 = 0;
-            BigInteger y_1 = 1;
-            BigInteger x = 0, y = 0;
+
             //Do the following while r > 1
-            do
-            {
-                //Set r to be the remainer of the divison a by b
-                r = a % b;
-                //Set q to be equal to the whole division of a by b
-                q = a / b;
-                //Set x: x_0 - qx_1
-                x = x_0 - q * x_1;
-                //Set  y: y_0 - qy_1
-                y = y_0 - q * y_1;
-                // Set x_0: x_1
-                x_0 = x_1;
-                // Set y_0: y_1
-                y_0 = y_1;
-                //Set x_1: x
-                x_1 = x;
-                //Set y_1: y
-                y_1 = y;
-                //Set a:b
-                a = b;
-                //Set b:r
-                b = r;
-            } while (r > 1);
+
+            //Set r to be the remainer of the divison a by b
+            //Set q to be equal to the whole division of a by b
+            //Set x: x_0 - qx_1
+            //Set  y: y_0 - qy_1
+            // Set x_0: x_1
+            // Set y_0: y_1
+            //Set x_1: x
+            //Set y_1: y
+            //Set a:b
+            //Set b:r
             // If r is 0 return the array {a, x, y}. In other cases return {r, x, y}. Note that order is critical.
-            if (r == 0)
-                result[0] = a;
-            else
-                result[0] = r;
-            result[1] = x;
-            result[2] = y;
-            return result;
+            throw new NotImplementedException();
         }
     }
 }
--- Algorithms/AlgorithmsCore/RSA.cs
+++ Algorithms/AlgorithmsCore/RSA.cs
@@ -5,62 +5,46 @@ namespace AlgorithmsCore
 {
     public static partial class Algorithms
     {
-        //private static readonly BigInteger p = BigInteger.Parse("12131072439211271897323671531612440428472427633701410925634549312301964373042085619324197365322416866541017057361365214171711713797974299334871062829803541");
-        //private static readonly BigInteger q = BigInteger.Parse("12027524255478748885956220793734512128733387803682075433653899983955179850988797899869146900809131611153346817050832096022160146366346391812470987105415233");
-        //private static readonly BigInteger e = 65537;
-        //private static readonly BigInteger d = BigInteger.Parse("89489425009274444368228545921773093919669586065884257445497854456487674839629818390934941973262879616797970608917283679875499331574161113854088813275488110588247193077582527278437906504015680623423550067240042466665654232383502922215493623289472138866445818789127946123407807725702626644091036502372545139713");
-
         public static Keys RSAGetKeys()
         {
             //Generate 2 big (preferably 100+digit) prime numbers p and q
-            var p = GetBigPrime(100);
-            var q = GetBigPrime(100);
             //n = p * q
             // Multiply the numbers and store them in n - modulus
-            var n = BigInteger.Multiply(p, q);
             //Calculate totient using the following formular
             //phi(n) = (p-1)*(q-1)
-            var totient = BigInteger.Multiply((p - 1), (q - 1));
 
-            var e = GetPublicKey(totient);
-            var d = GetPrivateKey(totient, e);
+            //var e = GetPublicKey(totient);
+            //var d = GetPrivateKey(totient, e);
             //return keys
-            return new Keys(e, d, n);
+            throw new NotImplementedException();
         }
         public static BigInteger GetPublicKey(BigInteger totient)
         {
             //Generate 50digit prime that is coprime with totient
             //Hint use GetBigPrime to generate the number
             //Hint use Euclidean Algorithm to test if numbers are coprime
-            var length = (50);
-            BigInteger e = 5;
-            do
-            {
-                e = GetBigPrime(length);
-                if (EuclideanAlgorithm(e, totient) == 1)
-                    return e;
-            } while (true);
+            throw new NotImplementedException();
+
         }
         public static BigInteger GetPrivateKey(BigInteger totient, BigInteger e)
         {
             //Use Modular Multiplicative Inverse to generate the private key
             //Pass totient and e to the method
             //If the resulting inverse is negative return the sum of the inverse and the totient
-            BigInteger[] d = new BigInteger[3];
-            d = ExtendedEuclideanAlgorithm(totient, e);
-            if (d[2] < 0)
-                d[2] = d[2] + totient;
-            return d[2];
+            throw new NotImplementedException();
+
         }
         public static BigInteger RSAEncrypt(BigInteger public_key, BigInteger n, BigInteger message)
         {
             //Use the ModPow to return the remainer of the (m)^public_key % n
-            return BigInteger.ModPow(message, public_key, n);
+            throw new NotImplementedException();
+
         }
         public static BigInteger RSADecrypt(BigInteger private_key, BigInteger n, BigInteger message)
         {
             //Use the ModPow to return the remainer of the (m)^private_key % n
-            return BigInteger.ModPow(message, private_key, n);
+            throw new NotImplementedException();
+
         }
     }
 
